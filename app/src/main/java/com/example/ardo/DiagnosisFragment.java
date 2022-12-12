319af2177ce0766a0dd4c8e137c916fb965d8eef
DELL <bezaleeljoshua@gmail.com>
2022-12-12 18:08:57 +0000

    Eighth Commit

--- app/src/main/java/com/example/ardo/DiagnosisFragment.java
+++ app/src/main/java/com/example/ardo/DiagnosisFragment.java
@@ -9,23 +9,21 @@ import androidx.fragment.app.Fragment;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
+ * Use the {@link DiagnosisFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class HomeFragment extends Fragment {
-
+public class DiagnosisFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
-    public HomeFragment() {
+    public DiagnosisFragment() {
         // Required empty public constructor
     }
 
@@ -35,11 +33,11 @@ public class HomeFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
+     * @return A new instance of fragment CameraFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
+    public static DiagnosisFragment newInstance(String param1, String param2) {
+        DiagnosisFragment fragment = new DiagnosisFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -60,13 +58,6 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-
+        return inflater.inflate(R.layout.fragment_diagnosis, container, false);
     }
-
-
-
-
-
-
 }
---
+++ app/src/main/java/com/example/ardo/Mainpageafterlogin.java
@@ -0,0 +1,75 @@
+package com.example.ardo;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class Mainpageafterlogin extends AppCompatActivity {
+
+    BottomNavigationView nav;
+    HomeFragment homeFragment = new HomeFragment();
+    TypesFragment typesFragment = new TypesFragment();
+    CausesFragment causesFragment=new CausesFragment();
+    FrameLayout frameLayout;
+
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainpageafterlogin);
+
+        nav = findViewById(R.id.nav);
+        frameLayout = findViewById(R.id.Logout);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        nav.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+
+                switch (item.getItemId()){
+                    case R.id.home1:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                        return true;
+                    case R.id.types:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, typesFragment).commit();
+                        return true;
+                    case R.id.causes:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, causesFragment).commit();
+                        return true;
+
+                }
+                return false;
+            }
+        });
+
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId()==R.id.Logout){
+            Fragment fragmentlogout = new LogoutFragment();
+            FragmentTransaction transactionlogout = getSupportFragmentManager().beginTransaction();
+            transactionlogout.replace(R.id.container, fragmentlogout);
+            transactionlogout.commit();
+        }
+        return true;
+    }
+}
